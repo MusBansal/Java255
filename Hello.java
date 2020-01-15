33a25e5488a12b16e24bdc26ac91549adb296243
MusBansal <47071235+MusBansal@users.noreply.github.com>
2020-01-15 17:23:54 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/****************************************************************************
+*Hello
+****************************************************************************
+Muskan Bansal
+January 15 2020
+CMSC 255 901
+****************************************************************************/
+public class Hello {
+
+    public static void main(String args[]){
+        System.out.println("Muskan Bansal");
+        System.out.println("Hello");
+        System.out.println("This is the first program for Java Lab");
+        System.out.println("CMSC 255 901");
+        System.out.println();
+    }
+}
